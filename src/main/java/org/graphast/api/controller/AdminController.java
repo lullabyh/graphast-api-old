fabbcb06cf0833c856ba993306f7faf9594d5033
Mariana <mariana.aparecida14@gmail.com>
2017-09-12 09:15:20 +0000

    Services implemented of delete and shortest path

--- src/main/java/org/graphast/api/controller/AdminController.java
+++ src/main/java/org/graphast/api/controller/AdminController.java
@@ -8,7 +8,9 @@ import org.graphast.app.GraphService;
 import org.graphast.config.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,7 +53,15 @@ public class AdminController {
 	}
 	
 	@GetMapping
-	public List<String> appNames() {
-		return Configuration.getAppNames();
+	public ResponseEntity<List<String>> appNames() {
+		return ResponseEntity.ok(Configuration.getAppNames());
+	}
+	
+	@DeleteMapping("/{app}")
+	public ResponseEntity<HttpStatus> delete(@PathVariable String app) {
+		logger.info("delete {}", app);
+		service.delete(app);
+		logger.info("Application " + app + " removed successfully");
+		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/org/graphast/api/controller/ShortestPathController.java
@@ -0,0 +1,47 @@
+package org.graphast.api.controller;
+
+import org.graphast.app.AppGraph;
+import org.graphast.config.Configuration;
+import org.graphast.model.Graph;
+import org.graphast.query.route.shortestpath.AbstractShortestPathService;
+import org.graphast.query.route.shortestpath.dijkstra.DijkstraConstantWeight;
+import org.graphast.query.route.shortestpath.model.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shortestPath")
+public class ShortestPathController {
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	
+	public ResponseEntity<String> test(String queryParam) {
+		
+		log.info(queryParam);
+		Configuration.setSelectedApp(queryParam);
+		return ResponseEntity.ok("App selecionado" + queryParam);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Path> shortestPath(Double lat1, Double long1, Double lat2, Double long2) {
+		
+		log.info("Shortest Path use Dijkstra ");
+		Graph graph = AppGraph.getGraph();
+		
+		log.info("get graph selected ");
+		
+		AbstractShortestPathService sp = new DijkstraConstantWeight(graph);
+		
+		log.info("Calculando path ");
+		long source = graph.getNodeId(lat1, long1);
+		long target = graph.getNodeId(lat2, long2);
+		Path path = sp.shortestPath(source, target);
+		log.info("Calculo terminado. . . ");
+		return ResponseEntity.ok(path);
+	}
+}
